10772b5e94c2eca7a371f042144863862cd12886
sandipkumarbhuyan <sandipbhuyan@gmail.com>
2018-02-12 16:05:27 +0530

    [TASK]: finsih the project

--- resources/views/donate.blade.php
+++ resources/views/donate.blade.php
@@ -101,4 +101,6 @@
         </div>
     </div>
 </form>
+<script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
 @endsection
