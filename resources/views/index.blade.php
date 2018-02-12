e2d26c0a42c24cd506f0011b28707faca8c5d2d3
sandipkumarbhuyan <sandipbhuyan@gmail.com>
2018-02-12 19:05:29 +0530

    [TASK]: Adding Pictures

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -25,6 +25,30 @@
                 </div>
             </div>
         </div>
+        <br>
+        <br>
+        <div class="row">
+            <div class="col-md-6">
+                <div class="reasons-col fadeIn">
+                    <img src="{{url('images/5.jpg')}}" alt="">
+                    <div class="on-hover hidden-xs">
+                        <p> The blood is red gold in time of saving a life.</p>
+                        <p>Share a little, care a little – Donate Blood.</p>
+                        <p>Five minutes of your time + 350 ml. of your blood = One life saved.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="reasons-col fadeIn">
+                    <img src="{{url('images/6.jpg')}}" alt="">
+                    <div class="on-hover hidden-xs">
+                        <p> To the young and healthy it’s no loss. To sick it’s hope of life. Donate Blood to give back life.</p>
+                        <p> The Blood Donor of today may be recipient of tomorrow.</p>
+                        <p> To the young and healthy it’s no loss. To sick it’s hope of life. Donate Blood to give back life.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
 
         <br>
     </div>
